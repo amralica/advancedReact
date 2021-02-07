5a783d2994aa162b8c176fc648da013894bf0bb3
amralica <amroali.ca@gmail.com>
2021-02-07 20:54:04 +0000

    #02 Next.js

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # advancedReact
+git add .
+git commit -m "#02 Next.js"
+git push -u origin main
---
+++ frontend/pages/index.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import Page from '../components/Page';
+
+export default function IndexPage() {
+  return (
+    <>
+      <p>Hello!</p>
+    </>
+  );
+}
